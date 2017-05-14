20a003c6d453d6f6bacbbbf5e01da053039d473e
MainShayne233 <shaynetremblay@hotmail.com>
2017-05-14 12:32:01 -0400

    basic component

---
+++ src/containers/AppContainer.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { browserHistory, Router } from 'react-router'
+import { Provider } from 'react-redux'
+import Counter from '../routes/Counter'
+
+class AppContainer extends Component {
+  static propTypes = {
+    store  : PropTypes.object.isRequired
+  }
+
+  shouldComponentUpdate () {
+    return false
+  }
+
+  render () {
+    const { store } = this.props
+    const CounterComponent = Counter(store)
+
+    return (
+      <Provider store={store}>
+        <div style={{ height: '100%' }}>
+          <CounterComponent/>
+        </div>
+      </Provider>
+    )
+  }
+}
+
+export default AppContainer
---
+++ src/routes/Counter/index.js
@@ -0,0 +1,8 @@
+import { injectReducer } from '../../store/reducers'
+
+export default (store) => {
+  const Counter = require('./containers/CounterContainer').default
+  const reducer = require('./modules/counter').default
+  injectReducer(store, { key: 'counter', reducer })
+  return Counter
+}
