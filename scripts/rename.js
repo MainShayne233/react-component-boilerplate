0edd6306c68e6f70108d746349daf85fda08a5fe
MainShayne233 <shaynetremblay@hotmail.com>
2017-05-14 21:05:57 -0400

    adds readme to rename script

--- scripts/rename.js
+++ scripts/rename.js
@@ -1,21 +1,34 @@
 const fs = require('fs')
 const path = require('path')
 const boilerplateName = 'react-component-boilerplate'
-const fileName = 'package.json'
+const packageDotJson = 'package.json'
+const readMe = 'README.md'
 const encoding = 'utf8'
 const dirname = path.resolve('.').split('/').reverse()[0]
 const gitUserName = 'MainShayne233'
 const description = 'starting place for creating a react component'
 
-fs.readFile(fileName, encoding, function(err, file) {
+const updatedReadMe = [
+  `# ${dirname}`,
+  'A cool React component',
+]
+
+fs.writeFile(readMe, updatedReadMe, encoding, (err) => {
+  if (err) throw(err)
+  console.log('Successfully updated README.md')
+})
+
+fs.readFile(packageDotJson, encoding, function(err, file) {
   const updatedPackageDotJson = file.split(boilerplateName)
                                     .join(dirname)
                                     .split(gitUserName)
                                     .join('yourGitUsername')
                                     .split(description)
                                     .join('A cool React component')
+
   fs.writeFile(fileName, updatedPackageDotJson, encoding, (err) => {
     if (err) throw(err)
-    console.log('Succesfully renamed app')
+    console.log('Succesfully updated package.json')
   })
+
 })
